057e4509cf44e67f6b293bf37e62f1a29e65ae98
Shubh-kumar1 <shubhamraj9493@gmail.com>
2025-10-13 15:24:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The New Line - Java/the-new-line-java.java
@@ -0,0 +1,12 @@
+class Geeks {
+
+    
+    static void printIndividualLine() {
+
+         System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+
+        
+    }
+}
