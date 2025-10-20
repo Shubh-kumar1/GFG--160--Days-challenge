0ebae8d4d436ea44fd544e0f372f4a87de602153
Shubh-kumar1 <shubhamraj9493@gmail.com>
2025-10-20 16:03:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 2/for-loop-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void utility(String s) {
+        
+        for(int i=0; i<s.length(); i=i+2){
+            char c = s.charAt(i);
+            
+            System.out.print(c);
+            
+        }
+        
+        
+    }
+}
